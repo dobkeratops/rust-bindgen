7b21da033cd7bf9b1b30209d47535aa473833a93
walter <dobkeratopsgit@gmail.com>
2013-07-17 17:54:58 +0100

    added missing files for testing cpp bindings

---
+++ test_bindgen_cpp.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "test_bindgen_cpp.h"
+
+
+
+#define OFS(base,member) ((size_t)(&(base->member))-(size_t)(base))
+extern "C" void Foo_bar(struct Foo* self, int x, float y) {
+	printf("C++ says sizeof *self=%d\n",sizeof(*self));
+	printf("C++ says offsetof x,y,z=%d,%d,%d\n",OFS(self,x),OFS(self,y),OFS(self,z));
+	printf("%x\n",self);
+	printf("hello from c++\n%s\n",__FUNCTION__);
+	self->bar(x,y);
+}
+extern "C" const char* Foo_getBaz(struct Foo* self, const void* src) {
+	return self->getBaz(src);
+}
+
+void Foo::bar(int ax, float ay) {
+	printf("%s ax=%d ay=%.3f\n",__FUNCTION__,ax,ay);
+	printf("thisptr = %x\n",this);
+	printf("Before store: %s this->x=%d this->y%.3f\n",__FUNCTION__,this->x,this->y);
+	this->x = ax;
+	this->y = ay;
+	printf("after store:%s this->x=%d this->y%.3f\n",__FUNCTION__,this->x,this->y);
+}
+const char* Foo::getBaz(const void* src) {
+	printf("%s %d %.3f\n",__FUNCTION__,this->x,this->y);
+}
+
+extern "C" const char* Nested_s_getOrange(struct Nested_s*, int x, float y) {
+	printf("hello from c++ %s\n",__FUNCTION__);
+	return	NULL;
+}
+
+extern "C" int foo(int, int, int) {
+	printf("hello from c++ %s\n",__FUNCTION__);
+	return	0;
+}
+
+extern "C" float* foo2(int, const char* txt[7], int*) {
+	return	NULL;
+}
+
+extern "C" void foo3(int) {
+
+}
+
---
+++ testcpp_from_rs.rs
@@ -0,0 +1,43 @@
+use std::libc::*;
+use testbindgencpp::*;
+use std::sys::*;	// for size_of
+mod testbindgencpp;
+mod cppemu;
+
+#[link_args = "-L."]
+#[link_args = "-lglut"]
+#[link_args = "-ltest_bindgen_cpp"]
+
+extern "C" {
+//	fn foo(a:c_int,b:c_int,c:c_int)->c_int;
+	fn glutInit();
+}
+
+unsafe fn lea<T>(a:*T)->uint {	a as uint }
+unsafe fn ofs<A,B>(a:*A,b:*B)->int {	a as int  - b as int  }
+
+fn main() {
+
+	let mut a=Struct_Foo{
+		x: 1.0,
+		y: 2.0,
+		z: 3.0,
+		name: 0 as *c_schar,
+		m_nested:Struct_Nested_s{
+			nx:0,ny:0
+		}
+	};
+	println(fmt!("size of Struct_Foo = %u\n", size_of::<Struct_Foo>()));
+	unsafe {
+		let px:*c_float=&a.x;
+		let pa:*Struct_Foo=&a;
+		println(fmt!("Rust says offset of x,y,z = %i,%i,%i a addr=%x, x,y,z addrs= %x,%x,%x\n", 
+				 ofs(&a.x,&a), ofs(&a.y,&a),ofs(&a.z,&a), lea(&a),lea(&a.x),lea(&a.y),lea(&a.z)
+			));
+		a.bar(3,4.0);
+		foo(1,2,3);
+	}
+	println("hello from rust");
+	
+}
+
